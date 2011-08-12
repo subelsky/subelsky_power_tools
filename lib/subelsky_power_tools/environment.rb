0fec625512df1e160eeb5c092f59e1a4e3292747
Mike Subelsky <github@mikeshop.net>
2011-08-12 15:46:53 -0400

    add a space

--- lib/subelsky_power_tools/environment.rb
+++ lib/subelsky_power_tools/environment.rb
@@ -2,7 +2,7 @@ module SubelskyPowerTools::Environment
   extend self
 
   def enforce_variables(*vals)
-    msg = "Must specify #{vals.map(&:upcase).join(",")}"
+    msg = "Must specify #{vals.map(&:upcase).join(", ")}"
 
     vals.each do |val|
       fail msg if ENV[val].to_s.strip.empty?

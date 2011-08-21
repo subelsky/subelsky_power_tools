120c4d87dec80220f1cbafdf89298ec187ac0667
Mike Subelsky <github@mikeshop.net>
2011-08-21 11:43:46 -0400

    create two versions of extract so env variables can be optional

--- lib/subelsky_power_tools/environment.rb
+++ lib/subelsky_power_tools/environment.rb
@@ -3,12 +3,22 @@ require "subelsky_power_tools"
 module SubelskyPowerTools::Environment
   extend self
 
+  def extract!(*keys)
+    get_keys(keys,true)
+  end
+
   def extract(*keys)
+    get_keys(keys,false)
+  end
+  
+  private
+
+  def get_keys(keys,raise_error_if_blank)
     keys.map! { |k| k.upcase.to_s }
     msg = "Must specify #{keys.join(", ")}"
 
     keys.inject([]) do |total,key|
-      fail msg if ENV[key].to_s.strip.empty?
+      fail msg if raise_error_if_blank && ENV[key].to_s.strip.empty?
       total << ENV[key]
     end
   end

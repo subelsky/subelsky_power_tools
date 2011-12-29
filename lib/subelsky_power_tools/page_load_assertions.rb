a0efe583b9c58f53149143aae65db54ee2ea2908
Mike Subelsky <github@mikeshop.net>
2011-12-29 09:59:31 -0500

    add page load assertions for capybara

---
+++ lib/subelsky_power_tools/page_load_assertions.rb
@@ -0,0 +1,17 @@
+# assertions for use with Capybara
+module PageLoadAssertons
+  def visit!(path,*args)
+    visit path,*args
+    should_load(path)
+  end
+
+  def should_load(path)
+    page.current_path.should == path
+    page.status_code.should == 200
+  end
+
+  def should_404(path)
+    page.current_path.should == path
+    page.status_code.should == 404
+  end
+end
--- lib/subelsky_power_tools/version.rb
+++ lib/subelsky_power_tools/version.rb
@@ -1,3 +1,3 @@
 module SubelskyPowerTools
-  VERSION = "1.2.1"
+  VERSION = "1.2.2"
 end

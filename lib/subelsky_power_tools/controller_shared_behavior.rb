dd2bf2802064555426c3cc63951368395b208894
Mike Subelsky <github@mikeshop.net>
2012-08-25 14:32:21 -0400

    allow shared controller behavior to work with nested controllers

--- lib/subelsky_power_tools/controller_shared_behavior.rb
+++ lib/subelsky_power_tools/controller_shared_behavior.rb
@@ -1,15 +1,22 @@
 # https://www.relishapp.com/rspec/rspec-core/v/2-10/docs/example-groups/shared-examples
 shared_examples "a protected plural controller" do |skip_actions|
+  # if you specify a "request options" method in your test, we use that when making requests.
+  # useful for testing nested controllers, e.g. if you were testing a comments controller
+  # that used nested routes like /:post_id/comments, you could do this:
+  #
+  # let(:request_options) { { post_id: 5 } }
+  let(:protected_plural_controller_options) { defined?(request_options) ? request_options : {} }
+
   describe "new", :if => described_class.instance_methods.include?(:new) && not(Array(skip_actions).include?(:new)) do
     it "requires signin" do
-      get :new
+      get :new, protected_plural_controller_options
       response.should redirect_to signin_redirect
     end
   end
 
   describe "edit", :if => described_class.instance_methods.include?(:edit) && not(Array(skip_actions).include?(:edit)) do
     def simple_get(obj_id = 1)
-      get :edit, { id: obj_id }
+      get :edit, protected_plural_controller_options.merge(id: obj_id)
     end
 
     it "requires signin" do
@@ -26,7 +33,7 @@ shared_examples "a protected plural controller" do |skip_actions|
 
   describe "show", :if => described_class.instance_methods.include?(:show) && not(Array(skip_actions).include?(:show)) do
     def simple_get(obj_id = 1)
-      get :show, { id: obj_id }
+      get :show, protected_plural_controller_options.merge(id: obj_id)
     end
 
     it "requires signin" do
@@ -50,14 +57,14 @@ shared_examples "a protected plural controller" do |skip_actions|
 
   describe "create", :if => described_class.instance_methods.include?(:create) && not(Array(skip_actions).include?(:create)) do
     it "requires signin" do
-      post :create
+      post :create, protected_plural_controller_options
       response.should redirect_to signin_redirect
     end
   end
 
   describe "update", :if => described_class.instance_methods.include?(:update) && not(Array(skip_actions).include?(:update)) do
     def simple_put(obj_id = 1,params = {})
-      put :update, params.merge(id: obj_id)
+      put :update, params.merge(id: obj_id).merge(protected_plural_controller_options)
     end
 
     it "requires signin" do
@@ -79,7 +86,7 @@ shared_examples "a protected plural controller" do |skip_actions|
 
   describe "delete", :if => described_class.instance_methods.include?(:destroy) && not(Array(skip_actions).include?(:destroy)) do
     def simple_delete(obj_id = 1)
-      delete :destroy, id: obj_id
+      delete :destroy, protected_plural_controller_options.merge(id: obj_id)
     end
 
     it "requires signin" do
@@ -97,44 +104,45 @@ shared_examples "a protected plural controller" do |skip_actions|
 end
 
 shared_examples "a protected singular controller" do |skip_actions|
+  let(:protected_singular_controller_options) { defined?(request_options) ? request_options : {} }
   describe "new", :if => described_class.instance_methods.include?(:new) && not(Array(skip_actions).include?(:new)) do
     it "requires signin" do
-      get :new
+      get :new, protected_singular_controller_options
       response.should redirect_to signin_redirect
     end
   end
 
   describe "edit", :if => described_class.instance_methods.include?(:edit) && not(Array(skip_actions).include?(:edit)) do
     it "requires signin" do
-      get :edit
+      get :edit, protected_singular_controller_options
       response.should redirect_to signin_redirect
     end
   end
 
   describe "show", :if => described_class.instance_methods.include?(:show) && not(Array(skip_actions).include?(:show)) do
     it "requires signin" do
-      get :show
+      get :show, protected_singular_controller_options
       response.should redirect_to signin_redirect
     end
   end
 
   describe "create", :if => described_class.instance_methods.include?(:create) && not(Array(skip_actions).include?(:create)) do
     it "requires signin" do
-      post :create
+      post :create, protected_singular_controller_options
       response.should redirect_to signin_redirect
     end
   end
 
   describe "update", :if => described_class.instance_methods.include?(:update) && not(Array(skip_actions).include?(:update)) do
     it "requires signin" do
-      put :update
+      put :update, protected_singular_controller_options
       response.should redirect_to signin_redirect
     end
   end
 
   describe "delete", :if => described_class.instance_methods.include?(:destroy) && not(Array(skip_actions).include?(:destroy)) do
     it "requires signin" do
-      delete :destroy
+      delete :destroy, protected_singular_controller_options
       response.should redirect_to signin_redirect
     end
   end

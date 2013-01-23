37afe79caedaccf8aeae3a26f222c62db99bbebe
Mike Subelsky <github@mikeshop.net>
2013-01-23 13:29:30 -0500

    add sidekiq assertions

---
+++ lib/subelsky_power_tools/sidekiq_assertions.rb
@@ -0,0 +1,35 @@
+RSpec::Matchers.define :have_queued_job do |*expected|
+  match do |actual|
+    actual.jobs.any? { |job| job["args"] == Array(expected) }
+  end
+
+  failure_message_for_should do |actual|
+    "expected that #{actual} would have a job queued with #{expected}"
+  end
+
+  failure_message_for_should_not do |actual|
+    "expected that #{actual} would not a have a job queued with #{expected}"
+  end
+
+  description do
+    "have a job queued with #{expected}"
+  end
+end
+
+RSpec::Matchers.define :have_queued_job_at do |at,*expected|
+  match do |actual|
+    actual.jobs.any? { |job| job["args"] == Array(expected) && job["at"].to_i == at.to_i }
+  end
+
+  failure_message_for_should do |actual|
+    "expected that #{actual} would have a job queued with #{expected} at time #{at}"
+  end
+
+  failure_message_for_should_not do |actual|
+    "expected that #{actual} would not a have a job queued with #{expected} at time #{at}"
+  end
+
+  description do
+    "have a job queued with #{expected} at time #{at}"
+  end
+end

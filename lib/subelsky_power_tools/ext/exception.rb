23f0783114ccbff376b29487c9e293b182ac1c29
Mike Subelsky <github@mikeshop.net>
2014-11-14 13:15:22 -0500

    add Exception#summary helper

--- lib/subelsky_power_tools/ext/exception.rb
+++ lib/subelsky_power_tools/ext/exception.rb
@@ -12,8 +12,13 @@ class Exception
   #
   # CREDIT: George Moschovitis
 
+  # @return [String] long description of the error including backtrace
   def detail
     return %{#{self.class.name}: #{message}\n#{backtrace.join("\n  ")}\n  LOGGED FROM: #{caller[0]}}
   end
 
+  # @return [String] exception class and message, useful for logging
+  def summary
+    "#{self.class.name}: #{message}"
+  end
 end
--- spec/lib/ext/exception_spec.rb
+++ spec/lib/ext/exception_spec.rb
@@ -2,11 +2,19 @@ require "spec_helper"
 require "subelsky_power_tools/ext/exception"
 
 describe Exception do
+  let(:exception) do
+    StandardError.new("something bad happened")
+  end
+
   it "reports details" do
     begin
-      raise StandardError.new("something bad happened")
+      raise exception
     rescue StandardError => e
       expect(e.detail).to match(/StandardError: something bad happened\n/)
     end
+  end
+
+  it "reports a summary" do
+    expect(exception.summary).to eq("StandardError: something bad happened")
   end
 end
